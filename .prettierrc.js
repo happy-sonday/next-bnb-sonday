748017dc6f146b2f196cec4aeda147ca32733e83
haapy-sonday <mrsmiler11062@gmail.com>
2021-06-26 08:52:53 +0900

    eslint&prettier 설정

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  singleQuote: false, // ingle 쿼테이션 사용 여부
+  semi: true, // 세미콜론 사용 여부
+  useTabs: false, // 탭 사용 여부
+  trailingComma: "all", // 여러 줄을 사용할 때, 후행 콤마 사용 방식
+  printWidth: 100, //줄 바꿈 할 폭 길이
+  arrowParens: "always", // 화살표 함수 괄호 사용 방식
+};
