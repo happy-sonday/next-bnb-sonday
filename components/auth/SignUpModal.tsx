a796f44ccf41ed08018a4e410631d25c587fb838
haapy-sonday <mrsmiler11062@gmail.com>
2021-06-27 16:19:48 +0900

    signup form 퍼블리싱

--- components/auth/SignUpModal.tsx
+++ components/auth/SignUpModal.tsx
@@ -1,15 +1,70 @@
 import styled from "styled-components";
 import React from "react";
+import palette from "../../styles/palette";
+import CloseXIcon from "../../public/static/svg/modal/modal_close_x_icon.svg";
+import PersonIcon from "../../public/static/svg/auth/person.svg";
+import MailIcon from "../../public/static/svg/auth/mail.svg";
+import OpenedEyeIcon from "../../public/static/svg/auth/opened_eye.svg";
+import ClosedEyeIcon from "../../public/static/svg/auth/closed_eye.svg";
 
 const Container = styled.div`
-  width: 500px;
-  height: 614px;
+  width: 568px;
+  padding: 32px;
+
   background-color: white;
   z-index: 11;
+  .modal-close-x-icon {
+    cursor: pointer;
+    display: block;
+    margin: 0 0 40px auto;
+  }
+
+  .input-wrapper {
+    position: relative;
+    margin-bottom: 16px;
+    input {
+      position: relative;
+      width: 100%;
+      height: 46px;
+      padding: 0 44px 0 11px;
+      border: 1px solid ${palette.gray_eb};
+      border-radius: 4px;
+      font-size: 16px;
+      outline: none;
+      ::placeholder {
+        color: ${palette.gray_76};
+      }
+    }
+    svg {
+      position: absolute;
+      right: 11px;
+      top: 16px;
+    }
+  }
 `;
 
 const SignUpModal: React.FC = () => {
-  return <Container>Sign Up</Container>;
+  return (
+    <Container>
+      <CloseXIcon className="modal-close-x-icon" />
+      <div className="input-wrapper">
+        <input placeholder="이메일 주소" name="email" type="email" />
+        <MailIcon />
+      </div>
+      <div className="input-wrapper">
+        <input placeholder="이름(예:길동)" />
+        <PersonIcon />
+      </div>
+      <div className="input-wrapper">
+        <input placeholder="성(예:홍)" />
+        <PersonIcon />
+      </div>
+      <div className="input-wrapper">
+        <input placeholder="비밀번호 설정하기" type="password" />
+        <OpenedEyeIcon />
+      </div>
+    </Container>
+  );
 };
 
 export default SignUpModal;
